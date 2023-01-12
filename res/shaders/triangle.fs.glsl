57c075ee65b796498711b896f5eca430f1e1c901
discodyer <cody23333@gmail.com>
2023-01-13 00:20:27 +0800

    ✒修改shader

--- res/shaders/triangle.fs.glsl
+++ res/shaders/triangle.fs.glsl
@@ -1,7 +1,9 @@
 #version 330 core
 out vec4 FragColor;
 
+in vec4 vertexColor; // 从顶点着色器传来的输入变量（名称相同、类型相同）
+
 void main()
 {
-    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
-} 
+    FragColor = vertexColor;
+}
